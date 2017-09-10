a6b004ccf1d883bffc6e125dfdfcc0ba02af1415
meldsza <melroynd@live.com>
2017-09-10 17:17:42 +0530

    add test

--- bot/modules.js
+++ bot/modules.js
@@ -9,34 +9,36 @@ const modules = [
     'ingame.js',
     'influx.js'
 ]
-const pm2 = require('pm2');
-let list = [];
-pm2.list((err, l) => { list = l; fork() })
-const fork = () => {
-    modules.map(n => {
-        const ch = list.find(l => l.name.toLowerCase().includes(n.toLowerCase()));
-        if (ch)
-            return pm2.restart(ch.name, (err) => {
-                if (!err)
-                    console.log("Restarted " + n)
-                else
-                    console.log(n + ": " + err)
-            })
-        else
-            pm2.start(process.cwd() + '/bot/modules/' + n, {
-                name: n,
-                maxRestarts: 1,
-                minUptime: 1600,
-                env: {
-                    'DISCORD_TOKEN': process.env.DISCORD_TOKEN,
-                    'REVIVE_API': process.env.REVIVE_API,
-                    'INFLUX_HOST': process.env.INFLUX_HOST
-                }
-            }, (err) => {
-                if (!err)
-                    console.log("Started " + n)
-                else
-                    console.log(n + ": " + err)
-            })
-    })
+if (!require('./../settings.json').test) {
+    const pm2 = require('pm2');
+    let list = [];
+    pm2.list((err, l) => { list = l; fork() })
+    const fork = () => {
+        modules.map(n => {
+            const ch = list.find(l => l.name.toLowerCase().includes(n.toLowerCase()));
+            if (ch)
+                return pm2.restart(ch.name, (err) => {
+                    if (!err)
+                        console.log("Restarted " + n)
+                    else
+                        console.log(n + ": " + err)
+                })
+            else
+                pm2.start(process.cwd() + '/bot/modules/' + n, {
+                    name: n,
+                    maxRestarts: 1,
+                    minUptime: 1600,
+                    env: {
+                        'DISCORD_TOKEN': process.env.DISCORD_TOKEN,
+                        'REVIVE_API': process.env.REVIVE_API,
+                        'INFLUX_HOST': process.env.INFLUX_HOST
+                    }
+                }, (err) => {
+                    if (!err)
+                        console.log("Started " + n)
+                    else
+                        console.log(n + ": " + err)
+                })
+        })
+    }
 }
