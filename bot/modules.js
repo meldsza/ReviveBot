b082695da34122ce42a81edc0da8df323cf5a7fb
meldsza <melroynd@live.com>
2017-09-10 10:48:02 +0530

    is modules,js even being required

--- bot/modules.js
+++ bot/modules.js
@@ -11,8 +11,11 @@ const modules = [
     'influx'
 ]
 const fork = require('child_process').fork;
-
+console.log("forking processes");
 let forks = modules.map(m => fork("./bot/modules/" + m, [], { stdio: 'inherit', env: process.env }));
 forks.map((p) => {
+    console.log('Fork pid: ' + p.pid)
     p.on('error', console.log);
+    p.on('close', console.log);
+    p.on('exit', console.log);
 })
