7be4e93ce4b44f06c7f55d00252e6c9847b655db
meldsza <melroynd@live.com>
2017-09-10 11:08:25 +0530

    use pm2 api

--- bot/modules.js
+++ bot/modules.js
@@ -10,12 +10,34 @@ const modules = [
     'saveAccessLog',
     'influx'
 ]
-const fork = require('child_process').fork;
-console.log("forking processes");
-let forks = modules.map(m => fork("./bot/modules/" + m, [], { stdio: 'inherit', env: process.env }));
-forks.map((p) => {
-    console.log('Fork pid: ' + p.pid)
-    p.on('error', console.log);
-    p.on('close', console.log);
-    p.on('exit', console.log);
-})
+const pm2 = require('pm2');
+let list = {}
+pm2.list((err, list) => { list = list; fork() })
+const fork = () => {
+    modules.map(n => {
+        const ch = list.find(l => l.name.toLowerCase().includes(n.toLowerCase()));
+        if (ch)
+            return pm2.restart(ch.name, (err) => {
+                if (!err)
+                    console.log("Restarted " + n)
+                else
+                    console.log(n + ": " + err)
+            })
+        else
+            pm2.start(cwd + '/bot/modules' + n, {
+                name: n,
+                maxRestarts: 1,
+                minUptime: 1600,
+                env: {
+                    'DISCORD_TOKEN': process.env.DISCORD_TOKEN,
+                    'REVIVE_API': process.env.REVIVE_API,
+                    'INFLUX_HOST': process.env.INFLUX_HOST
+                }
+            }, (err) => {
+                if (!err)
+                    console.log("Started " + n)
+                else
+                    console.log(n + ": " + err)
+            })
+    })
+}
