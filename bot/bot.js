df51f44802f3885a09ce365332523a9a3100ec0e
meldsza <melroynd@live.com>
2017-09-18 20:18:37 +0530

    fix bot

--- bot/bot.js
+++ bot/bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const settings = require('./../settings.json');
-const client = new Discord.Client({disableEveryone:true});
+const client = new Discord.Client({ disableEveryone: true });
 
-var dt = process.env.DISCORD_TOKEN || settings.token || process.argv[2];
+var dt = settings.token || process.env.DISCORD_TOKEN || process.argv[2];
 
 if (!dt) {
     console.log('required DISCORD_TOKEN env variable or argument');
