cd23305fb667be3df3db3f416cc0bd4c44372de5
meldsza <melroynd@live.com>
2017-09-18 19:44:46 +0530

    fix modules

--- bot/modules.js
+++ bot/modules.js
@@ -16,21 +16,25 @@ if (!require('./../settings.json').test) {
     pm2.list((err, l) => { list = l; fork() })
     const fork = () => {
         modules.map(n => {
-            pm2.restart(__dirname + '/modules/' + n, {
-                name: n,
-                maxRestarts: 1,
-                minUptime: 1600,
-                env: {
-                    'DISCORD_TOKEN': process.env.DISCORD_TOKEN,
-                    'REVIVE_API': process.env.REVIVE_API,
-                    'INFLUX_HOST': process.env.INFLUX_HOST
-                }
-            }, (err) => {
-                if (!err)
-                    console.log("Started " + n)
-                else
-                    console.log(n + ": " + err)
-            })
+            const ch = list.find(l => l.name.toLowerCase().includes(n.toLowerCase()));
+            if (!ch)
+                pm2.start(__dirname + '/modules/' + n, {
+                    name: n,
+                    maxRestarts: 1,
+                    minUptime: 1600,
+                    env: {
+                        'DISCORD_TOKEN': process.env.DISCORD_TOKEN,
+                        'REVIVE_API': process.env.REVIVE_API,
+                        'INFLUX_HOST': process.env.INFLUX_HOST
+                    }
+                }, (err) => {
+                    if (!err)
+                        console.log("Started " + n)
+                    else
+                        console.log(n + ": " + err)
+                })
+            else
+                pm2.restart(n);
         })
     }
 }
