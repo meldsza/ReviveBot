3dd1c4448b588ab2f13a0e6ebbc2db594bec8ab0
meldsza <melroynd@live.com>
2017-09-22 19:35:40 +0530

    update isMentioned is removed

--- bot/listeners/digest.js
+++ bot/listeners/digest.js
@@ -14,13 +14,13 @@ module.exports = message => {
         params = params.split(settings.delimiter || ' ');
         cmd = params.shift().trim();
     }
-    else if (message.isMentioned(message.client.user)) {
+    else if (message.mentions.has(message.client.user)) {
         /**Remove mention and extract params */
         params = message.content.split(settings.delimiter || ' ');
         params = params.filter(p => !p.includes(message.client.user.id))
         cmd = params.shift().trim();
     }
     //execute command
-    if(cmd)
+    if (cmd)
         commands.execute(cmd.toLowerCase(), params, message)
 }
